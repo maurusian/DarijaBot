dc2019855ef0a6368312a82e38a2ad5f3511e82e
Ideophagous <antontheog@gmail.com>
2022-02-23 22:23:42 +0100

    devloped welcome personality

---
+++ Task 16 - Welcome/run_bot_all.bat
@@ -0,0 +1,3 @@
+welcome.py login -all -a 3 10 11 15 828 829 1 0 14 8 9 12 13 6 7 4 5 2
+
+pause
--- Task 16 - Welcome/welcome.py
+++ Task 16 - Welcome/welcome.py
@@ -3,7 +3,9 @@ import random
 from copy import deepcopy
 from params import MONTHS
 from datetime import datetime, timezone
-from arywikibotlib import getOnlyArticles
+from sys import argv
+import os
+#from arywikibotlib import getOnlyArticles
 
 
 ADMIN_LIST = ["Ideophagous","Reda benkhadra","Anass Sedrati"]
@@ -15,6 +17,48 @@ ACCT_SAVE_MESSAGE = "ترحيب ب خدايمي"
 IP_SAVE_MESSAGE = "ترحيب ب أيپي"
 
 ARTICLE_NAMESPACE = 0
+
+LAST_RUN_FILE = "last_run.txt"
+
+DATE_FORMAT = "%Y-%m-%d %H:%M"
+
+#EARLIEST_TIME = datetime.strptime("2000-01-01 00:00",DATE_FORMAT)
+
+NAMESPACES = {0:'Articles'
+             ,1:'Article discussions'
+             ,2:'Users'
+             ,3:'User discussions'
+             ,4:'Project'
+             ,5:'Project discussion'
+             ,6:'File'
+             ,7:'File discussion'
+             ,8:'Mediawiki'
+             ,9:'Mediawiki discussion'
+             ,10:'Template'
+             ,11:'Template discussion'
+             ,12:'Help'
+             ,13:'Help discussion'
+             ,14:'Categories'
+             ,15:'Category discussion'
+             ,828:'Module'
+             ,829:'Module discussion'
+             ,'any':'any'
+             }
+
+def get_last_run_datetime():
+    if not os.path.exists(LAST_RUN_FILE):
+        with open(LAST_RUN_FILE,'w') as f:
+            return None
+
+    with open(LAST_RUN_FILE,'r') as f:
+        datetime_str = f.read().strip()
+
+    return datetime.strptime(datetime_str,DATE_FORMAT)
+
+def write_run_time():
+    with open(LAST_RUN_FILE,'w') as f:
+        f.write(pywikibot.Timestamp.now().strftime(DATE_FORMAT))
+    
 
 def get_random_admin():
     seed = int(datetime.now().strftime("%H%M%S%f"))
@@ -30,53 +74,86 @@ def get_time_string():
     return " "+raw_time_parts[1][:-4]+"، "+date_parts[2]+" "+MONTHS[int(date_parts[1])-1]["ary_name"]+" "+date_parts[0]+" (UTC)"
 
     
-"""
-if len(argv)>3:
-    local_args = argv[3:]
-else:
-    local_args = None
-"""
-local_args = 0 
-if local_args is not None:
-    site = pywikibot.Site()
-    """
-    if LAST_PAGES_OPTION in option_string:
-                
-        #load last changed
-        last_changes = site.recentchanges(reverse=True,bot=False,top_only=True)
-        #create page pool
-        #NEXT: check other potential last_change types
-
-        pool = [pywikibot.Page(site, item['title']) for item in last_changes]
-
-                
+if __name__ == '__main__':
+    print(len(argv))
+    if len(argv)>2:
+        if len(argv) > 3:
+            local_args = argv[3:]
     else:
+        local_args = None
 
-        #load all pages on the article namespace, default option
-        pool = site.allpages(namespace=ARTICLE_NAMESPACE)
-    """
-    pool = site.allpages(namespace=ARTICLE_NAMESPACE) #getOnlyArticles(site) #
-    pool_size = len(list(deepcopy(pool)))
-    print('Pool size: '+str(pool_size))
-    i = 1
-    for page in pool:
-        print('*********'+str(i)+'/'+str(pool_size))
+    #local_args = 0 
+    if local_args is not None:
+        site = pywikibot.Site()
+        namespaces = []
+        pools = []
+        if local_args[0] == '-l':
+            last_run_time = get_last_run_datetime()
+            print(last_run_time)
+            print("running for last changed pages")
+            #load last changed
+            last_changes = site.recentchanges(reverse=True,bot=False,top_only=True,start=last_run_time)
+            #create page pool
+            #NEXT: check other potential last_change types
+
+            pools = [[pywikibot.Page(site, item['title']) for item in last_changes]]
+            namespaces = ['any']
+
+        elif len(local_args) > 1 and local_args[0] == '-a':
+            print([int(ns) for ns in local_args[1:]])
+            #print(EARLIEST_TIME)
+            
+            for ns in [int(ns) for ns in local_args[1:]]:
+                pools.append(site.allpages(namespace=ns))
+                namespaces.append(ns)
+            
+        else:
+            print("running for all articles")
+            #load all pages on the article namespace, default option
+            pools = [site.allpages(namespace=ARTICLE_NAMESPACE)]
+            namespaces = [0]
         
-        contributors = dict(page.contributors())
+        for pool in pools:
+            print("Runnng for namespaces: "+NAMESPACES[namespaces[pools.index(pool)]])
+            pool_size = len(list(deepcopy(pool)))
+            print('Pool size: '+str(pool_size))
+            i = 1
+            for page in pool:
+                print('*********'+str(i)+'/'+str(pool_size))
+                
+                contributors = dict(page.contributors())
 
-        for contributor in contributors.keys():
-            user = pywikibot.User(site,contributor)
-            user_talk_page = user.getUserTalkPage()
-            if user_talk_page.text == "":
-                now = datetime.now()
-                time = get_time_string()
-                admin = get_random_admin()
-                if user.isRegistered():
-                    WELCOME = ACCT_WELCOME_TAG.replace("{admin}",admin).replace("{time}",time)
-                    SAVE_MESSAGE = ACCT_SAVE_MESSAGE
-                else:
-                    WELCOME = IP_WELCOME_TAG.replace("{admin}",admin).replace("{time}",time)
-                    SAVE_MESSAGE = IP_SAVE_MESSAGE
-                user_talk_page.text = WELCOME
-                user_talk_page.save(SAVE_MESSAGE)
-        i+=1
+                for contributor in contributors.keys():
+                    user = pywikibot.User(site,contributor)
+                    user_talk_page = user.getUserTalkPage()
+                    if user_talk_page.text == "":
+                        #now = datetime.now()
+                        time = get_time_string()
+                        admin = get_random_admin()
+                        if user.isRegistered():
+                            WELCOME = ACCT_WELCOME_TAG.replace("{admin}",admin).replace("{time}",time)
+                            SAVE_MESSAGE = ACCT_SAVE_MESSAGE
+                        else:
+                            WELCOME = IP_WELCOME_TAG.replace("{admin}",admin).replace("{time}",time)
+                            SAVE_MESSAGE = IP_SAVE_MESSAGE
+                        user_talk_page.text = WELCOME
+                        user_talk_page.save(SAVE_MESSAGE)
+                    else:
+                        talk_page_contributors = dict(user_talk_page.contributors())
+                        #print(page.title())
+                        #print(talk_page_contributors)
+                        if len(talk_page_contributors.keys()) == 1 and list(talk_page_contributors.keys())[0] == 'MenoBot':
+                            
+                            time = get_time_string()
+                            admin = get_random_admin()
+                            if user.isRegistered():
+                                WELCOME = ACCT_WELCOME_TAG.replace("{admin}",admin).replace("{time}",time)
+                                SAVE_MESSAGE = ACCT_SAVE_MESSAGE
+                            else:
+                                WELCOME = IP_WELCOME_TAG.replace("{admin}",admin).replace("{time}",time)
+                                SAVE_MESSAGE = IP_SAVE_MESSAGE
+                            user_talk_page.text = WELCOME
+                            user_talk_page.save(SAVE_MESSAGE)
+                i+=1
+                
+    write_run_time()
