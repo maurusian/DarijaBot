43e05cd2302ca3872b6fd8e0b3f2e9b1d9649d1c
Ideophagous <antontheog@gmail.com>
2023-06-06 12:34:50 +0200

    made some change on the scheduling task, but it's not working yet

--- Task 19 - scheduler/scheduler.py
+++ Task 19 - scheduler/scheduler.py
@@ -1,4 +1,11 @@
 import pywikibot
+from datetime import datetime
+from random import randint
+from subprocess import Popen, PIPE
+
+PARAM_PAGE_TITLE = "ميدياويكي:عطاشة 19.json"
+
+DARIJABOT_ROOT = "C:\\Users\\anton\\Documents\\scripts\\DarijaBot\\"
 
 #abstract class?
 class Program:
@@ -32,3 +39,43 @@ class Program:
 
     def write_to_error_log(self,message):
         pass
+
+    def extract_dicts(self,page):
+        return [eval(el.split(']]')[0]) for el in page.text.split('[[')[1:]]
+
+        
+
+if __name__=='__main__':
+    site = pywikibot.Site()
+    param_page = pywikibot.Page(site,PARAM_PAGE_TITLE)
+
+    prog = Program()
+
+    TASK_LIST = prog.extract_dicts(param_page)
+
+    for task in TASK_LIST:
+        JOBID = str(task['tasknumber'])+datetime.now().strftime('%d%m%y')
+        print(JOBID)
+        print(TASK_LIST[-1]['taskname'])
+
+    #process = Popen(TASK_LIST[0]['program'].strip(), stdout=PIPE, stderr=PIPE)
+
+    if TASK_LIST[-1]['bot'] == 'Darijabot':
+        root_folder = DARIJABOT_ROOT
+
+    task_folder = root_folder + TASK_LIST[-1]['program_loc']
+    task_file = task_folder + TASK_LIST[-1]['program_name']
+    with open(task_file,'r') as prog:
+        instr = prog.read().replace('pause','').strip()
+        print(instr)
+
+    p = Popen(instr+JOBID, cwd=task_folder, shell=True)
+
+
+    stdout, stderr = p.communicate()
+
+    print(stdout)
+
+
+    while True:
+        DAY_COUNT_SINCE_LAST_LAUNCH_TASK2 = 0
